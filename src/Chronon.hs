2a95af460475387b52f160e45e8970524009d36e
xsoufiane <soufiane.maguerra@commercetools.de>
2021-02-23 23:39:24 +0100

    feat(Chronon): add synchronicity and concurrency

--- src/Chronon.hs
+++ src/Chronon.hs
@@ -2,10 +2,12 @@ module Time where
 
 data Chronon
 
--- | Precedence relation
+-- | Relations
 (<) :: Chronon -> Chronon -> Boolean
+concurrent :: Chronon -> Chronon -> Boolean
+synchronous :: Chronon -> Chronon -> Boolean
 
--- | Chronon Laws :
+-- | Precedence Laws :
 forall (t1 :: Chronon) (t2 :: Chronon) (t3 :: Chronon).
   t1 < t2 && t2 < t3 => t1 < t3 -- ^ Transitivity
 
@@ -17,3 +19,14 @@ forall (t1 :: Chronon) (t2 :: Chronon).
 
 forall (t1 :: Chronon) (t2 :: Chronon).
   t1 < t2 || t2 < t1 || t1 == t2 -- ^ Linearity
+  
+-- | Concurrency, Synchronicity  Laws :
+forall (t1 :: Chronon) (t2 :: Chronon).
+  t1 `concurrent` t2 = not t1 < t2 && not t2 < t1 -- ^ concurrency
+forall (t1 :: Chronon) (t2 :: Chronon).
+  t1 `synchronous` t2 = (forall (t :: Chronon). t1 < t => t2 < t) && not t1 < t2 -- ^ Synchronicity
+forall (t1 :: Chronon) (t2 :: Chronon).
+  t1 `synchronous` t2 = (forall (t :: Chronon). t1 < t => t2 < t) && not t1 < t2 -- ^ Synchronicity
+
+
+
