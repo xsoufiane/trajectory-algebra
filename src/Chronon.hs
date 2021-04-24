ce640a028d19850d8ae641cf5568626ea78da867
xsoufiane <soufiane.maguerra@commercetools.de>
2021-04-24 16:00:59 +0200

    feat(Chronon): add after, remove concurrency

--- src/Chronon.hs
+++ src/Chronon.hs
@@ -1,52 +1,77 @@
-module Time where
+module Chronon where
 
 data Chronon
 
 -- | Observations
-(<) :: Chronon -> Chronon -> Boolean
-betweenness :: Chronon -> Chronon -> Chronon -> Boolean
-concurrent :: Chronon -> Chronon -> Boolean
+(<) :: Chronon -> Chronon -> Boolean -- ^ Precedence
+
+(>) :: Chronon -> Chronon -> Boolean -- ^ After
+
+(===) :: Chronon -> Chronon -> Boolean -- ^ Identity
+
 synchronous :: Chronon -> Chronon -> Boolean
+
+betweenness :: Chronon -> Chronon -> Chronon -> Boolean
 
 -- | Precedence Laws :
 forall (x :: Chronon).
   not x < x -- ^ Irreflexitivity
 
 forall (x :: Chronon) (y :: Chronon).
-  x < y => not x < y -- ^ Asymmetry
-  
+  x < y => not y < x -- ^ Asymmetry
+
 forall (x :: Chronon) (y :: Chronon) (z :: Chronon).
   x < y && y < z => x < z -- ^ Transitivity
 
 forall (x :: Chronon) (y :: Chronon).
-  x < y || y < x || x == y -- ^ Linearity
+  x < y || x > y || x === y -- ^ Linearity
   
--- | Betweenness  Laws :
-forall (x :: Chronon) (y :: Chronon) (z :: Chronon).
-  betweenness(x, y, z) <=> (y < x &&  x < z)  || (z < x && x < y)
+forall (x :: Chronon) (y :: Chronon)
+  x < y = not x > y && not x === y
   
--- | Concurrency  Laws :
+-- | Identity Laws :
 forall (x :: Chronon).
-  x `concurrent` x -- ^ reflexitivity
+  x === x -- ^ reflexitivity
 
 forall (x :: Chronon) (y :: Chronon).
-  x `concurrent` y <=> y `concurrent` x  -- ^ Symmetry
+  x === y => y === x -- ^ Symmetry
+  
+forall (x :: Chronon) (y :: Chronon) (z :: Chronon).
+  x === y && y === z => x === z -- ^ Transitivity
   
 forall (x :: Chronon) (y :: Chronon).
-  x `concurrent` y = not x < y && not y < x
+  x === y => not x < y && not x > y
   
+-- | After Operator Laws :
+forall (x :: Chronon).
+  not x > x -- ^ Irreflexitivity
+
+forall (x :: Chronon) (y :: Chronon).
+  x > y => not y > x -- ^ Asymmetry
+
+forall (x :: Chronon) (y :: Chronon) (z :: Chronon).
+  x > y && y > z => x > z -- ^ Transitivity
+
+forall (x :: Chronon) (y :: Chronon).
+  x > y = not x < y && not x === y
+
 -- | Synchronicity  Laws :
+forall (x :: Chronon) (y :: Chronon) (t :: Chronon).
+  x `synchronous` y = (x < t => y < t) && not x < y -- ^ Definition
+  
 forall (x :: Chronon).
   x `synchronous` x -- ^ reflexitivity
   
-forall (x :: Chronon) (y :: Chronon).
-  x `synchronous` y <=> y `synchronous` x  -- ^ Symmetry
-  
 forall (x :: Chronon) (y :: Chronon) (z :: Chronon).
   x `synchronous` y && y `synchronous` z => x `synchronous` z -- ^ Transitivity
+
+forall (x :: Chronon) (y :: Chronon).
+  x > y => not x `synchronous` y
   
 forall (x :: Chronon) (y :: Chronon).
-  x `synchronous` y = (forall (t :: Chronon). x < t => y < t) && not t1 < t2 -- ????
-
-
-
+  x === y => x `synchronous` y
+  
+-- | Betweenness  Laws :
+forall (x :: Chronon) (y :: Chronon) (z :: Chronon).
+  betweenness x y z = (y < x &&  x < z)  || (z < x && x < y)
+  
